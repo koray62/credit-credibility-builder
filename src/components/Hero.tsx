0b734527b17b59bcd2d46aefe6c006a861bd3aae
koray62 <57031011+koray62@users.noreply.github.com>
2025-04-05 17:34:56 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -107,7 +107,7 @@ const Hero: React.FC = () => {
                     <svg className="h-5 w-5 text-green-500 mr-1" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                     </svg>
-                    <span className="text-gray-700">Hedef Puan: 1300+</span>
+                    <span className="text-gray-700">Hedef Puan: 1420+</span>
                   </div>
                   <span className="text-gray-700">12 ay içinde</span>
                 </div>
