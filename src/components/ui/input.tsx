7e76e63fda2290463b97dc6ae0f9e4a6693c2071
koray62 <57031011+koray62@users.noreply.github.com>
2025-05-07 10:49:34 +0300

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -17,6 +17,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
     )
   }
 )
-Input.displayName = "Input"
 
-export { Input }
+export const Input = React.forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
+  (props, ref) => <input ref={ref} {...props} />
+);
+Input.displayName = 'Input';
