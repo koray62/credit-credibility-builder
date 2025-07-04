0898ec663c57e3bf8f7cc8adbd86eda4e99ff55c
koray62 <57031011+koray62@users.noreply.github.com>
2025-07-04 11:51:35 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,7 +48,7 @@ const Hero: React.FC = () => {
             </h1>
             
             <p className="text-gray-700 text-lg md:text-xl mb-8 animate-fade-in" style={{ animationDelay: "0.2s" }}>
-              Findeks notunuz yoksa endişelenmeyin. SkorUp ile kredi puanınızı güvenli, risksiz ve ücretsiz bir şekilde oluşturmaya başlayın. Kredi notunun hayatınıza katacağı avantajlardan faydalanın.
+              SkorUp avantaj programı sayesinde Findeks notunuz pek çok farklı alanda size özel avantajlar sunuyor. SkorUp ile kredi puanınızı güvenli, risksiz ve ücretsiz bir şekilde oluşturmaya başlayın. Aramıza katılın, kredi notunun hayatınıza katacağı avantajlardan faydalanın.
             </p>
             
             <div className="flex flex-col sm:flex-row gap-4 animate-fade-in" style={{ animationDelay: "0.4s" }}>
